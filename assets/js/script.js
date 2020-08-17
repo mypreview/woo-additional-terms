515e5b1d847ed847f5aaea032d4857558e0a33ba
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-08-18 01:52:16 +0300

    Toggle the visibility of the additional terms content

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+/**
+ * Toggles the visibility of the additional terms content.
+ *
+ * @since       1.3.3
+ */
+jQuery( function ( $ ) {
+	const wooTermsToggle = {
+		init() {
+			$( document.body ).on( 'click', 'a.woo-additional-terms__link', this.toggleTerms );
+		},
+
+		toggleTerms() {
+			if ( $( '.woo-additional-terms__content' ).length ) {
+				$( '.woo-additional-terms__content' ).slideToggle( function () {
+					const linkToggle = $( '.woo-additional-terms__link' );
+
+					if ( $( '.woo-additional-terms__content' ).is( ':visible' ) ) {
+						linkToggle.addClass( 'woo-additional-terms__link--open' );
+						linkToggle.removeClass( 'woo-additional-terms__link--closed' );
+					} else {
+						linkToggle.removeClass( 'woo-additional-terms__link--open' );
+						linkToggle.addClass( 'woo-additional-terms__link--closed' );
+					}
+				} );
+
+				return false;
+			}
+		},
+	};
+
+	wooTermsToggle.init();
+} );
