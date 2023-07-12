793d63bf2af5c1633b2bfcc4881978e501f975a9
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-07-12 19:23:02 +0300

    The plugin internationalization class

---
+++ src/I18n.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The plugin internationalization class.
+ *
+ * @author MyPreview (Github: @mahdiyazdani, @gooklani, @mypreview)
+ *
+ * @since 1.0.0
+ *
+ * @package woo-additional-terms
+ */
+
+namespace Woo_Additional_Terms;
+
+/**
+ * Loads and defines the internationalization files for this plugin.
+ */
+abstract class I18n {
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public static function textdomain() {
+
+		$domain = 'woo-additional-terms';
+		// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound
+		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
+
+		// Load the translation file for current language.
+		load_textdomain( $domain, trailingslashit( WP_LANG_DIR ) . "{$domain}/{$domain}-{$locale}.mo" );
+		load_plugin_textdomain( $domain, false, woo_additional_terms()->service( 'file' )->dirname() . '/languages/' );
+	}
+}
