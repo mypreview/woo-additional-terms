f4c3d817864a33b79c06dd27a7a14af531737040
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-07-12 19:18:56 +0300

    The plugin template manager class

---
+++ src/TemplateManager.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The plugin template manager class.
+ *
+ * @author MyPreview (Github: @mahdiyazdani, @gooklani, @mypreview)
+ *
+ * @since 1.6.0
+ *
+ * @package woo-additional-terms
+ */
+
+namespace Woo_Additional_Terms;
+
+/**
+ * Class TemplateManager.
+ */
+class TemplateManager {
+
+	/**
+	 * Render the template.
+	 *
+	 * @since 1.6.0
+	 *
+	 * @param string $template_name The template name.
+	 * @param array  $args          The template arguments.
+	 *
+	 * @return void
+	 */
+	public function echo_template( $template_name, $args = array() ) {
+
+		// Supports internal WooCommerce caching.
+		wc_get_template(
+			$template_name,
+			$args,
+			'',
+			trailingslashit( woo_additional_terms()->service( 'file' )->plugin_path( 'templates' ) )
+		);
+	}
+}
