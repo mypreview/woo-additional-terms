d2a5a523a91ff5fe3c580a5c6f8b275c956decc0
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-07-12 19:59:12 +0300

    The plugin installer class

---
+++ src/Installer.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The plugin installer class.
+ *
+ * @author MyPreview (Github: @mahdiyazdani, @gooklani, @mypreview)
+ *
+ * @since 1.0.0
+ *
+ * @package woo-additional-terms
+ */
+
+namespace Woo_Additional_Terms;
+
+use Woo_Additional_Terms\Enhancements;
+
+/**
+ * The plugin installer class.
+ */
+class Installer {
+
+	/**
+	 * Activate the plugin.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public static function activate() {
+
+		// Add the activation timestamp, if not already added.
+		woo_additional_terms()->service( 'options' )->add_usage_timestamp();
+	}
+
+	/**
+	 * Deactivate the plugin.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public static function deactivate() {
+
+		delete_transient( Enhancements\Rate::TRANSIENT_NAME );
+	}
+}
