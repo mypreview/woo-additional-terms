08bad85f38501b3d886f8704da0d04c72157f1a8
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-07-14 12:21:25 +0300

    Plugin settings registerer

---
+++ src/Settings/Register.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Plugin settings registerer.
+ *
+ * @author MyPreview (Github: @mahdiyazdani, @gooklani, @mypreview)
+ *
+ * @since 1.6.0
+ *
+ * @package woo-additional-terms
+ */
+
+namespace Woo_Additional_Terms\Settings;
+
+/**
+ * Register plugin settings fields.
+ */
+class Register {
+
+	/**
+	 * Setup hooks and filters.
+	 *
+	 * @since 1.6.0
+	 *
+	 * @return void
+	 */
+	public function setup() {
+
+		add_filter( 'woocommerce_get_settings_pages', array( $this, 'settings' ) );
+	}
+
+	/**
+	 * We will add our settings pages using the following filter, so that the code that
+	 * being used to hook into that filter is init by a filter later than `wp_loaded`.
+	 *
+	 * @since 1.6.0
+	 *
+	 * @param array $settings Setting pages.
+	 *
+	 * @return array
+	 */
+	public function settings( $settings ) {
+
+		// Add our settings page.
+		$settings[] = woo_additional_terms()->service( 'settings' );
+		return $settings;
+	}
+}
