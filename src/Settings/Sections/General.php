7e27397156bf90d38705d46453c1c7ff39e66bff
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-07-15 15:11:29 +0300

    Add "Display Action" field

--- src/Settings/Sections/General.php
+++ src/Settings/Sections/General.php
@@ -50,10 +50,33 @@ class General extends Section {
 			'page' => array(
 				'name'     => _x( 'Terms Page', 'settings field name', 'woo-additional-terms' ),
 				'desc'     => _x( 'If you define a "Terms" page the customer will be asked if they accept additional terms when checking out.', 'settings field description', 'woo-additional-terms' ),
-				'type'     => 'single_select_page',
-				'class'    => 'wc-enhanced-select-nostd',
+				'type'     => 'single_select_page_with_search',
+				'class'    => 'wc-page-search',
 				'css'      => 'min-width:300px;',
 				'id'       => 'woo_additional_terms_options[page]',
+				'args'     => array(
+					'exclude' =>
+						array(
+							wc_get_page_id( 'cart' ),
+							wc_get_page_id( 'checkout' ),
+							wc_get_page_id( 'myaccount' ),
+						),
+				),
+				'desc_tip' => true,
+				'autoload' => false,
+			),
+			'action' => array(
+				'name'     => _x( 'Display Action', 'settings field name', 'woo-additional-terms' ),
+				'desc'     => _x( 'The options are &quot;Embed&quot; (same behavior as WooCommerce T&C), &quot;Modal&quot; (opens page content in a modal), or &quot;New tab&quot; (opens linked page in a new tab).', 'settings field description', 'woo-additional-terms' ),
+				'type'     => 'select',
+				'class'    => 'wc-enhanced-select',
+				'id'       => 'woo_additional_terms_options[action]',
+				'options'  => array(
+					'embed'  => _x( 'Embed above the checkbox', 'settings field option', 'woo-additional-terms' ),
+					'modal'  => _x( 'Open page content in a modal', 'settings field option', 'woo-additional-terms' ),
+					'newtab' => _x( 'Open linked page in a new tab', 'settings field option', 'woo-additional-terms' ),
+				),
+				'default'  => 'embed',
 				'desc_tip' => true,
 				'autoload' => false,
 			),
@@ -70,7 +93,7 @@ class General extends Section {
 					'cols' => 50,
 				),
 			),
-			'notice_info' => array(
+			'smart_tag_info' => array(
 				'type' => 'info',
 				'text' => sprintf( /* translators: 1: Open paragraph tag, 2: Terms smart tag, 3: Close paragraph tag. */
 					esc_html_x( '%1$sYou may utilize the %2$s smart tag to automatically populate the terms page name in your notice content.%3$s', 'settings field description', 'woo-additional-terms' ),
@@ -79,20 +102,19 @@ class General extends Section {
 					'</p>'
 				),
 			),
-			'action' => array(
-				'name'     => _x( 'Display Action', 'settings field name', 'woo-additional-terms' ),
-				'desc'     => _x( 'The options are &quot;Embed&quot; (same behavior as WooCommerce T&C), &quot;Modal&quot; (opens page content in a modal), or &quot;New tab&quot; (opens linked page in a new tab).', 'settings field description', 'woo-additional-terms' ),
-				'type'     => 'select',
-				'class'    => 'wc-enhanced-select',
-				'id'       => 'woo_additional_terms_options[action]',
-				'options'  => array(
-					'embed'  => _x( 'Embed above the checkbox', 'settings field option', 'woo-additional-terms' ),
-					'modal'  => _x( 'Open page content in a modal', 'settings field option', 'woo-additional-terms' ),
-					'newtab' => _x( 'Open linked page in a new tab', 'settings field option', 'woo-additional-terms' ),
+			'error' => array(
+				'name'              => _x( 'Error Message', 'settings field name', 'woo-additional-terms' ),
+				'desc'              => _x( 'Add an error message to show if the customer does not accept the additional terms.', 'settings field description', 'woo-additional-terms' ),
+				'placeholder'       => _x( 'Please accept the additional terms to continue.', 'settings field placeholder', 'woo-additional-terms' ),
+				'default'           => _x( 'Please accept the additional terms to continue.', 'settings field default', 'woo-additional-terms' ),
+				'type'              => 'textarea',
+				'id'                => 'woo_additional_terms_options[error]',
+				'desc_tip'          => true,
+				'autoload'          => false,
+				'custom_attributes' => array(
+					'rows' => 4,
+					'cols' => 50,
 				),
-				'default'  => 'embed',
-				'desc_tip' => true,
-				'autoload' => false,
 			),
 			'section_end' => array(
 				'type' => 'sectionend',
