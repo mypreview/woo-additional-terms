a37920788b7e298ca7c197a9b1b2890f6f413490
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-07-15 15:09:09 +0300

    Early return

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,13 @@
 ( function ( $ ) {
 	'use strict';
 
-	const wat = {
+	const script = {
 		cache() {
 			this.vars = {};
 			this.els = {};
 			this.vars.selector = 'woo-additional-terms';
-			this.vars.embed = `${ this.vars.selector }__link`;
-			this.vars.content = `${ wat.vars.selector }__content`;
+			this.vars.embed = `${ this.vars.selector }__link[data-action="embed"]`;
+			this.vars.content = `${ script.vars.selector }__content`;
 			this.vars.openClassName = `${ this.vars.selector }__link--open`;
 			this.vars.closeClassName = `${ this.vars.selector }__link--closed`;
 			this.vars.wrapper = `.woocommerce-terms-and-conditions-wrapper.${ this.vars.selector }`;
@@ -17,10 +17,6 @@
 
 		init() {
 			this.cache();
-			this.events();
-		},
-
-		events() {
 			$( document.body ).on( 'click', `a.${ this.vars.embed }`, this.handleEmbedToggle );
 		},
 
@@ -28,24 +24,23 @@
 			event.preventDefault();
 
 			const $this = $( this );
-			const $wrapper = $this.closest( wat.vars.wrapper );
-			const $content = $wrapper.find( `.${ wat.vars.content }` );
+			const $wrapper = $this.closest( script.vars.wrapper );
 
-			if ( $wrapper.length ) {
-				$content.slideToggle( function () {
-					if ( $content.is( ':visible' ) ) {
-						$this.addClass( wat.vars.openClassName );
-						$this.removeClass( wat.vars.closeClassName );
-					} else {
-						$this.removeClass( wat.vars.openClassName );
-						$this.addClass( wat.vars.closeClassName );
-					}
-				} );
-
+			if ( ! $wrapper.length ) {
 				return false;
 			}
+
+			const $content = $wrapper.find( `> .${ script.vars.content }` );
+
+			$content.slideToggle( function () {
+				const isVisible = $content.is( ':visible' );
+				$this.toggleClass( script.vars.openClassName, isVisible );
+				$this.toggleClass( script.vars.closeClassName, ! isVisible );
+			} );
+
+			return false;
 		},
 	};
 
-	wat.init();
+	script.init();
 } )( jQuery );
