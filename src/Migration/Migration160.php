798106097b65865612a37ad46f3405c160227380
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-07-17 17:52:20 +0300

    Migration for the version 1.6.0

---
+++ src/Migration/Migration160.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Migration for the version 1.6.0.
+ *
+ * @author MyPreview (Github: @mahdiyazdani, @gooklani, @mypreview)
+ *
+ * @since 1.6.0
+ *
+ * @package woo-additional-terms
+ */
+
+namespace Woo_Additional_Terms\Migration;
+
+/**
+ * Migration class.
+ */
+class Migration160 extends Migration {
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 1.6.0
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+
+		// Parent constructor.
+		parent::__construct( '1.6.0' );
+	}
+
+	/**
+	 * Do the migration.
+	 *
+	 * @since 1.6.0
+	 *
+	 * @return void
+	 */
+	public function migrate_callback() {
+
+		// Bailout, if not eligible.
+		if ( ! $this->is_eligible() ) {
+			return;
+		}
+
+		woo_additional_terms()->service( 'options' )->update(
+			array(
+				'status'   => '1',
+				'required' => 'yes',
+				'page'     => wc_clean( get_option( '_woo_additional_terms_page_id', '' ) ),
+				'notice'   => wc_clean( get_option( '_woo_additional_terms_notice', '' ) ),
+				'error'    => wc_clean( get_option( '_woo_additional_terms_error', '' ) ),
+			)
+		);
+
+		// Mark the migration as complete.
+		$this->complete();
+	}
+}
