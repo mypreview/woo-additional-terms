6977e5f9ccfa655d4076338604ed431c012d4713
Sajedeh Gooklani <gookalani@mail.com>
2023-07-24 19:55:04 +0300

    Update option descriptions

--- src/Settings/Sections/General.php
+++ src/Settings/Sections/General.php
@@ -32,11 +32,11 @@ class General extends Section {
 				'id'   => 'woo-additional-terms-general',
 				'type' => 'title',
 				'name' => _x( 'Woo Additional Terms', 'settings section name', 'woo-additional-terms' ),
-				'desc' => _x( 'Close your store temporarily by scheduling your vacation time. While your shop will remain online and accessible to visitors, new order operations will pause, and your checkout will be disabled.', 'settings field description', 'woo-additional-terms' ),
+				'desc' => _x( 'Add an extra “I agree” checkbox to your checkout page, giving users the opportunity to explicitly agree to specific terms before completing their purchase. Save time, reduce disputes, and create a better shopping experience.', 'settings field description', 'woo-additional-terms' ),
 			),
 			'status' => array(
 				'name'     => _x( 'Status', 'settings field name', 'woo-additional-terms' ),
-				'desc'     => _x( 'The options are &quot;Active&quot; (enable this terms checkbox), or &quot;Disabled&quot; (deactivate (draft) this terms checkbox).', 'settings field description', 'woo-additional-terms' ),
+				'desc'     => _x( 'Choose “Active” to enable the terms checkbox during checkout, allowing customers to agree to the specified terms before purchase. Select “Disabled” to deactivate (draft) the terms checkbox, hiding it from the checkout process.', 'settings field description', 'woo-additional-terms' ),
 				'type'     => 'select',
 				'class'    => 'wc-enhanced-select',
 				'id'       => 'woo_additional_terms_options[status]',
@@ -49,7 +49,7 @@ class General extends Section {
 			),
 			'page' => array(
 				'name'     => _x( 'Terms Page', 'settings field name', 'woo-additional-terms' ),
-				'desc'     => _x( 'If you define a "Terms" page the customer will be asked if they accept additional terms when checking out.', 'settings field description', 'woo-additional-terms' ),
+				'desc'     => _x( 'Search and select the desired terms page to link it with the checkbox.', 'settings field description', 'woo-additional-terms' ),
 				'type'     => 'single_select_page_with_search',
 				'class'    => 'wc-page-search',
 				'css'      => 'min-width:300px;',
@@ -66,15 +66,15 @@ class General extends Section {
 				'autoload' => false,
 			),
 			'action' => array(
-				'name'     => _x( 'Display Action', 'settings field name', 'woo-additional-terms' ),
-				'desc'     => _x( 'The options are &quot;Embed&quot; (same behavior as WooCommerce T&C), &quot;Modal&quot; (opens page content in a modal), or &quot;New tab&quot; (opens linked page in a new tab).', 'settings field description', 'woo-additional-terms' ),
+				'name'     => _x( 'Link Action', 'settings field name', 'woo-additional-terms' ),
+				'desc'     => _x( 'This field provides you with options to control how the terms link behaves when clicked. The “Embed” option displays the content above the checkbox. Choosing “Modal” opens the content in a modal window, while “New Tab” opens the linked page in a new browser tab.', 'settings field description', 'woo-additional-terms' ),
 				'type'     => 'select',
 				'class'    => 'wc-enhanced-select',
 				'id'       => 'woo_additional_terms_options[action]',
 				'options'  => array(
-					'embed'  => _x( 'Embed above the checkbox', 'settings field option', 'woo-additional-terms' ),
-					'modal'  => _x( 'Open page content in a modal', 'settings field option', 'woo-additional-terms' ),
-					'newtab' => _x( 'Open linked page in a new tab', 'settings field option', 'woo-additional-terms' ),
+					'embed'  => _x( 'Embed above checkbox', 'settings field option', 'woo-additional-terms' ),
+					'modal'  => _x( 'Open in modal', 'settings field option', 'woo-additional-terms' ),
+					'newtab' => _x( 'Open in new tab', 'settings field option', 'woo-additional-terms' ),
 				),
 				'default'  => 'embed',
 				'desc_tip' => true,
@@ -82,7 +82,7 @@ class General extends Section {
 			),
 			'notice' => array(
 				'name'              => _x( 'Terms Text', 'settings field name', 'woo-additional-terms' ),
-				'desc'              => _x( 'Add some text about your store additional terms to show during checkout.', 'settings field description', 'woo-additional-terms' ),
+				'desc'              => _x( 'Enter the desired text that will be displayed alongside the checkbox, ensuring clarity and transparency when customers agree to the specified terms during the checkout process.', 'settings field description', 'woo-additional-terms' ),
 				'placeholder'       => _x( 'I have read and agree to the website {{additional-terms}}', 'settings field placeholder', 'woo-additional-terms' ),
 				'type'              => 'textarea',
 				'id'                => 'woo_additional_terms_options[notice]',
@@ -96,7 +96,7 @@ class General extends Section {
 			'smart_tag_info' => array(
 				'type' => 'info',
 				'text' => sprintf( /* translators: 1: Open paragraph tag, 2: Terms smart tag, 3: Close paragraph tag. */
-					esc_html_x( '%1$sYou may utilize the %2$s smart tag to automatically populate the terms page name in your notice content.%3$s', 'settings field description', 'woo-additional-terms' ),
+					esc_html_x( '%1$sUse the %2$s smart tag in the Terms Text to automatically display the linked terms page title in your checkbox label.%3$s', 'settings field description', 'woo-additional-terms' ),
 					'<p class="description">',
 					'<code>{{additional-terms}}</code>',
 					'</p>'
