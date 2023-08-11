16c35dec94dfeab31a83ec6fa41f4f31284c9e98
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-08-11 12:15:50 +0300

    feat: Show the terms acceptance on the order page

---
+++ src/Admin/Order.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * The WooCommerce order extensions.
+ *
+ * @author MyPreview (Github: @mahdiyazdani, @gooklani, @mypreview)
+ *
+ * @since 1.6.1
+ *
+ * @package woo-additional-terms
+ */
+
+namespace Woo_Additional_Terms\Admin;
+
+use WC_Order;
+
+/**
+ * Class Order.
+ */
+class Order {
+
+	/**
+	 * The meta key for additional terms.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	const META_KEY = '_woo_additional_terms';
+
+	/**
+	 * Setup hooks and filters.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function setup() {
+
+		add_action( 'woocommerce_admin_order_data_after_billing_address', array( $this, 'show_terms_acceptance' ) );
+	}
+
+	/**
+	 * Show the terms acceptance on the order page.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param WC_Order $order The order object.
+	 *
+	 * @return void
+	 */
+	public function show_terms_acceptance( $order ) {
+
+		woo_additional_terms()->service( 'template_manager' )->echo_template(
+			'order/terms-acceptance.php',
+			array(
+				'value' => $order->get_meta( self::META_KEY ),
+			)
+		);
+	}
+}
