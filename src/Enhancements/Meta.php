636115768ea63e00e5d5c279a5a12021ead44780
Mahdi Yazdani <mahdiyazdani@msn.com>
2023-08-27 16:08:24 +0300

    refactor: Remove plugin_basename variable

--- src/Enhancements/Meta.php
+++ src/Enhancements/Meta.php
@@ -19,30 +19,16 @@ use Woo_Additional_Terms\Helper;
 class Meta {
 
 	/**
-	 * The plugin basename.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @var string
-	 */
-	private $plugin_basename;
-
-	/**
 	 * Setup hooks and filters.
 	 *
 	 * @since 1.0.0
 	 *
-	 * @param string $plugin_basename The plugin basename.
-	 *
 	 * @return void
 	 */
-	public function setup( $plugin_basename ) {
-
-		// Set the plugin basename.
-		$this->plugin_basename = $plugin_basename;
+	public function setup() {
 
 		add_filter( 'plugin_row_meta', array( $this, 'meta_links' ), 10, 2 );
-		add_filter( "plugin_action_links_{$this->plugin_basename}", array( $this, 'action_links' ) );
+		add_filter( 'plugin_action_links', array( $this, 'action_links' ), 10, 2 );
 	}
 
 	/**
@@ -58,7 +44,7 @@ class Meta {
 	public function meta_links( $links, $file ) {
 
 		// Return early if not on the plugin page.
-		if ( $this->plugin_basename !== $file ) {
+		if ( ! $this->is_this_plugin( $file ) ) {
 			return $links;
 		}
 
@@ -90,11 +76,17 @@ class Meta {
 	 *
 	 * @since 1.0.0
 	 *
-	 * @param array $links Plugin table/item action links.
+	 * @param array  $links Plugin table/item action links.
+	 * @param string $file  Path to the plugin file relative to the plugins' directory.
 	 *
 	 * @return array
 	 */
-	public function action_links( $links ) {
+	public function action_links( $links, $file ) {
+
+		// Leave early if the filter is not for this plugin.
+		if ( ! $this->is_this_plugin( $file ) ) {
+			return $links;
+		}
 
 		$plugin_links   = array();
 		$plugin_links[] = sprintf( /* translators: 1: Open anchor tag, 2: Close anchor tag. */
@@ -105,15 +97,32 @@ class Meta {
 			),
 			'</a>'
 		);
-		$plugin_links[] = sprintf( /* translators: 1: Open anchor tag, 2: Close anchor tag. */
-			esc_html_x( '%1$sSettings%2$s', 'plugin settings page', 'woo-additional-terms' ),
-			sprintf(
-				'<a href="%s">',
-				esc_url( Helper\Settings::page_uri() )
-			),
-			'</a>'
-		);
+
+		if ( current_user_can( 'manage_woocommerce' ) ) {
+			$plugin_links[] = sprintf( /* translators: 1: Open anchor tag, 2: Close anchor tag. */
+				esc_html_x( '%1$sSettings%2$s', 'plugin settings page', 'woo-additional-terms' ),
+				sprintf(
+					'<a href="%s">',
+					esc_url( Helper\Settings::page_uri() )
+				),
+				'</a>'
+			);
+		}
 
 		return array_merge( $plugin_links, $links );
+	}
+
+	/**
+	 * Check if the current plugin is the one we are looking for.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $file Path to the plugin file relative to the plugins' directory.
+	 *
+	 * @return bool
+	 */
+	private function is_this_plugin( $file ) {
+
+		return woo_additional_terms()->service( 'file' )->plugin_basename() === $file;
 	}
 }
